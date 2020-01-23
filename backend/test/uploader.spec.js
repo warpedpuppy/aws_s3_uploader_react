36f7e267b31837baab2dbd937c6ef59fcc9fe3a9
ted <ted@warpedpuppy.com>
2020-01-23 15:29:03 -0500

    worked on backend unit test

--- backend/test/uploader.spec.js
+++ backend/test/uploader.spec.js
@@ -1,7 +1,7 @@
 const app = require('../src/app')
 var File = require("file-class");
 describe('uploader endpoint', function() {
-    it(`uploads and then deletes image`, () => {
+    it(`receives object with signed request from endpoint`, () => {
 
         let file = new File();
         file.name = `IMG_7548.jpeg`;
@@ -11,12 +11,14 @@ describe('uploader endpoint', function() {
         return supertest(app)
           .get(`/api/uploader/sign-s3?file-name=${file.name}&file-type=${file.type}`)
           .expect(res => {
-              
-            let { signedRequest } = JSON.parse(res.text);
+            expect(res.text).to.be.a("string")
+            let responseJSON = JSON.parse(res.text);
+            expect(responseJSON).to.have.keys("signedRequest", "url")
 
+            // let { signedRequest } = responseJSON;
             //  return supertest(app)
             //     .put(signedRequest)
-            //     .attach('image', filePath)
+            //     .attach('file', filePath)
             //     .then(result => console.log(result))
             //     .catch(error => console.error(error))
           })
