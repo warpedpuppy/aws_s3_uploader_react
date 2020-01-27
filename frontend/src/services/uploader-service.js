62e5d6130d2269af500dd72f337915d9fe46c52b
ted <ted@warpedpuppy.com>
2020-01-27 10:34:21 -0500

    moved oncomplete outside of uploader service

--- frontend/src/services/uploader-service.js
+++ frontend/src/services/uploader-service.js
@@ -5,18 +5,18 @@ const UploaderService = {
     const { files } = document.getElementById(id);
     const file = files[0];
     if (file === null) return alert('No file selected.');
-    let res = await this.getSignedRequest(file);
+    const res = await this.getSignedRequest(file);
     return res;
   },
   async getSignedRequest(file) {
-    let result = await fetch(`${Config.API_ENDPOINT}/uploader/sign-s3?file-name=${file.name}&file-type=${file.type}`)
-    let resultJSON = await result.json();
-    let { signedRequest } = resultJSON;
-    let uploadResult = await this.uploadFile(file, signedRequest);
+    const result = await fetch(`${Config.API_ENDPOINT}/uploader/sign-s3?file-name=${file.name}&file-type=${file.type}`)
+    const resultJSON = await result.json();
+    const { signedRequest } = resultJSON;
+    const uploadResult = await this.uploadFile(file, signedRequest);
     return uploadResult;
   },
   async uploadFile(file, signedRequest) {
-    let result = await fetch(signedRequest, {
+    const result = await fetch(signedRequest, {
       method: "PUT",
       headers:  { 'content-type': file.type },
       body: file
